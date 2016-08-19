6756e02b9ac65ab3e794dee5e94797f01349d1e6
WINDOWS420\Windows <memedacc1>
2016-08-20 00:30:59 +0200

    test

---
+++ IllyaHook/IllyaHook/DllMain.cc
@@ -0,0 +1,25 @@
+/* PROJECT IllyaHook by MRX - KUNO [ started : 20-08-16 ] */
+
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+DWORD WINAPI InterfaceInit(LPVOID IpThreadParam) 
+{
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
+	cout << __DATE__ << endl;
+	cout << "-IllyaHook by MRX & Kuno" << endl;
+	return 1;
+}
+
+
+BOOL WINAPI DllMain(HINSTANCE pInstance, DWORD dwReason, LPVOID IpReversed) 
+{
+	if (dwReason != DLL_PROCESS_ATTACH)
+		return TRUE;
+	CreateThread(nullptr, NULL, InterfaceInit, nullptr, NULL, nullptr);
+	return TRUE;
+}
