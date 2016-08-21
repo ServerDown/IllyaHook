c677b7ada1983fabbe23f4a8a80416cc93648ae5
ServerDown <telescope30@protonmail.com>
2016-08-21 01:32:51 +0000

    Update and rename Includes.h to Common.h

---
+++ IllyaHook/IllyaHook/Common.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2016 Mrx
+
+// Safeguard - (#pragma once) is nearly only supported in Visual Studio, not c++ standard. 
+#ifndef ILLYAHOOK_COMMON_H_
+#define ILLYAHOOK_COMMON_H_
+
+#define WIN32_LEAN_AND_MEAN // To speed the build process
+
+// #pragma warning(disable:4099)
+// #pragma warning(disable:4005)
+// #pragma warning(disable:4305)
+
+#include <Windows.h>
+#include <iostream>
+
+#endif // ILLYAHOOK_COMMON_H_
